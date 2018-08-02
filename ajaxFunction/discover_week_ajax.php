be89c92c363c852204c0262aadecba48de597478
Alexandre Guillemine <alex.guillemine@gmail.com>
2018-08-02 15:25:37 +0200

    changement d'exp du mail de bienvenue après essai

--- ajaxFunction/discover_week_ajax.php
+++ ajaxFunction/discover_week_ajax.php
@@ -416,7 +416,7 @@ function ajaxAfterSubmissionEssai()
         $profResponsable = utf8_encode($profResponsable);
         
         $expeMg = new \spamtonprof\stp_api\stpExpeManager();
-        $expe = $expeMg->get("alexandre@spamtonprof.com");
+        $expe = $expeMg->get("equipe@spamtonprof.com");
         $smtpMg = new \spamtonprof\stp_api\SmtpServerManager();
         $smtp = $smtpMg->get(array(
             "ref_smtp_server" => $smtpMg::smtp2Go
@@ -425,7 +425,7 @@ function ajaxAfterSubmissionEssai()
         if ($envoiEleve) {
             $body_eleve = file_get_contents(ABSPATH . "wp-content/plugins/spamtonprof/emails/bienvenue-essai-eleve.html");
             $body_eleve = str_replace("[prof-responsable]", $profResponsable, $body_eleve);
-            $smtp->sendEmail("Bienvenue " . $eleve->getPrenom(), $eleve->getEmail(), $body_eleve, $expe->getEmail(), $expe->getFrom_name(),true);
+            $smtp->sendEmail("Bienvenue " . $eleve->getPrenom(), $eleve->getEmail(), $body_eleve, $expe->getEmail(), "Alex de SpamTonProf",true);
         }
         
         if ($envoiParent) {
@@ -433,7 +433,7 @@ function ajaxAfterSubmissionEssai()
             $body_parent = str_replace("[prof-responsable]", $profResponsable, $body_parent);
             $body_parent = str_replace("[prenom-eleve]", $eleve->getPrenom(), $body_parent);
             
-            $smtp->sendEmail("Bienvenue " . $proche->getPrenom(), $proche->getEmail(), $body_parent, $expe->getEmail(), $expe->getFrom_name(),true);
+            $smtp->sendEmail("Bienvenue " . $proche->getPrenom(), $proche->getEmail(), $body_parent, $expe->getEmail(), "Alex de SpamTonProf",true);
         }
         
         echo (json_encode($retour));
