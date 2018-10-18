33278da6ddbfef934ba3b454ab14807c952270fc
Yop-La <alex.guillemine@gmail.com>
2018-10-18 23:24:38 +0200

    1.1.6.4.3 : fix traitement des communes lbc invalides

--- hook/putAsideLbcCommune.php
+++ hook/putAsideLbcCommune.php
@@ -1,16 +1,5 @@
 <?php
-use spamtonprof\stp_api\AccountManager;
-use spamtonprof\stp_api\EleveManager;
-use spamtonprof\stp_api\ClasseManager;
-use spamtonprof\stp_api\GmailLabelManager;
-use spamtonprof\stp_api\GmailLabel;
-use spamtonprof\stp_api\NbEmailManager;
-use spamtonprof\stp_api\GetResponseManager;
-use spamtonprof\getresponse_api\CampaignManager;
-use spamtonprof\stp_api\LbcProcessManager;
-use Hashids\Hashids;
-
-require_once (dirname(__FILE__) . '/wp-config.php');
+require_once (dirname(dirname(dirname(dirname(dirname(__FILE__))))) . '/wp-config.php');
 $wp->init();
 $wp->parse_request();
 $wp->query_posts();
@@ -21,9 +10,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-error_reporting(- 1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
@@ -33,7 +22,9 @@ header("Pragma: no-cache");
  * appelé par un template zenno lorsque qu'une annonce n'est pas reconnu par leboncoin
  * le script se charge de mettre la colonne lbc à true ne pas sélectionner lors de prochaines publications
  */
-$commune = $_POST['commune'];
+$commune = 'toulelknefe56v4zvezvvv 1 2121 e';
+echo($commune);
+
 
 $commune = explode(" ", $commune);
 
--- spamtonprof.php
+++ spamtonprof.php
@@ -20,7 +20,7 @@
  *
  *
  *
- * Version: 1.1.6.4.2
+ * Version: 1.1.6.4.3
  * 
  * 
  * Author: yopla
