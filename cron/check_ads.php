fa73b80f58d7b7ae6ff745be8ae4affb17ad849a
Alexandre Guillemine <alex.guillemine@gmail.com>
2019-01-05 22:03:19 +0100

    cron to check lbc ads every night

---
+++ cron/check_ads.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * pour contrôler les annonces en ligne sur leboncoin ( en prod )
+ *
+ * tourne tous les jours pendant la nuit
+ *
+ *
+ */
+require_once (dirname(dirname(dirname(dirname(dirname(__FILE__))))) . "/wp-config.php");
+$wp->init();
+$wp->parse_request();
+$wp->query_posts();
+$wp->register_globals();
+$wp->send_headers();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$lbc = new \spamtonprof\stp_api\LbcProcessManager();
+
+$lbc->checkAds(100);
