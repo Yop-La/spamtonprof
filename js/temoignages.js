2bb4a68da6f2b6c9e3e74fd84aef2416f1ff4a78
Alexandre Guillemine <alex.guillemine@gmail.com>
2019-01-21 12:47:16 +0100

    1.1.7.4.0 : remove fin timer essai

--- js/temoignages.js
+++ js/temoignages.js
@@ -1,41 +1,6 @@
 jQuery( document ).ready( function( jQuery ) {
 
 
-	//debut timer essai	
-
-
-	var timerEssai = localStorage.getItem("timerEssai");
-	console.log('timerEssai');
-	console.log(timerEssai);
-
-	var printTimer = true;
-
-	if(!timerEssai){
-
-		var timerEssai = new Date().getTime() + 5*24*60*60*1000;	
-		localStorage.setItem("timerEssai", timerEssai);
-
-	}
-
-	jQuery("#band-message").html('<div id = "promo_essai">7 jours d\'essai offerts dans la matière de votre choix </div><br> <div id = "expiration_essai"> Expire dans <span id = "counter-essai"></span><div>');
-
-	jQuery("#counter-essai").countdown(timerEssai, {elapse: true})
-	.on('update.countdown', function(event) {
-		var el = jQuery(this);
-		if (event.elapsed) {
-			hideMessage();
-		} else {
-			el.html(event.strftime('%D jours %H h %M min %S s'));
-			if(printTimer){
-				jQuery("#top-message").removeClass("hide");
-				window.scrollTo(0, 0);
-				printTimer = false;
-			}
-
-		}
-	});
-
-	//	fin timer essai
 
 
 
--- spamtonprof.php
+++ spamtonprof.php
@@ -20,7 +20,7 @@
  *
  *
  *
- * Version: 1.1.7.3.9
+ * Version: 1.1.7.4.0
  *
  *
  * Author: yopla
