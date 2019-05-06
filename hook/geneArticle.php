aad0761489151e791d2e5a20e9acf404f4f3c226
Alexandre Guillemine <alex.guillemine@gmail.com>
2019-05-06 22:21:57 +0200

    1.1.8.0.4
    to have messages in slack log

--- hook/geneArticle.php
+++ hook/geneArticle.php
@@ -23,24 +23,33 @@ header("Pragma: no-cache");
 $password = $_GET["password"];
 $keyword = $_GET["keyword"];
 
+$start = microtime(true);
 
+$msgs = [];
+$slack = new \spamtonprof\slack\Slack();
 if ($password == CRON_KEY) {
     
-    $url_serp = "https://www.serprobot.com/api/v1/api.php?api_key=" . SERP_ROBOT_KEY . "&action=get_serps&region=www.google.fr&keyword=".urlencode($keyword)."&device=desktop&hl=fr";
+    $msgs[] = 'Article généré';
+    $msgs[] = 'mot clé: ' . $keyword;
+    
+    $url_serp = "https://www.serprobot.com/api/v1/api.php?api_key=" . SERP_ROBOT_KEY . "&action=get_serps&region=www.google.fr&keyword=" . urlencode($keyword) . "&device=desktop&hl=fr";
     $urls = url_get_contents($url_serp);
     
     $urls = json_decode($urls);
-
     
-    if(property_exists($urls, 'error')){
+    
+    if (property_exists($urls, 'error')) {
         
-        $slack = new \spamtonprof\slack\Slack();
-        $slack->sendMessages('log-spam-google', array('Erreur fatale lors de la génération d\'un article: ',$urls->error));
+        $slack->sendMessages('log-spam-google', array(
+            'Erreur fatale lors de la génération d\'un article: ',
+            $urls->error
+        ));
         die();
-        
     }
     
-    $urls = $urls -> serps;
+    $urls = $urls->serps;
+    $msgs[] = 'Nb urls: ' . count($urls);
+    
     
     $urlParser = new \spamtonprof\stp_api\UrlParser();
     
@@ -53,6 +62,9 @@ if ($password == CRON_KEY) {
     $corpus_body = implode(" ", $texts);
     $corpus_title = implode(" ", $titles);
     
+    $msgs[] = 'extracted text size: ' . strlen($corpus_body);
+    $msgs[] = 'extracted title size: ' . strlen($corpus_title);
+    
     $textGenerator = new \spamtonprof\stp_api\TextGenerator();
     $text = $textGenerator->generate_text($corpus_body, 20000, 1);
     $title = $textGenerator->generate_text($corpus_title, 1000, 1);
@@ -61,12 +73,26 @@ if ($password == CRON_KEY) {
     serializeTemp($title, "/title");
     serializeTemp($images, "/images");
     
+    $msgs[] = 'Nb images: ' . count($images);
     
     $articleGenerator = new \spamtonprof\stp_api\ArticleGenerator();
     $article = $articleGenerator->generate_article($images, $text, $title);
     
-    echo ($articleGenerator->get_article());
+    $article = $articleGenerator->get_article();
+    $msgs[] = 'Post size: ' . strlen($article);
+    
+    $time_elapsed_secs = microtime(true) - $start;
+    
+    $msgs[] = 'Execution time: ' . round($time_elapsed_secs,2) . ' secondes ';
+    $msgs[] = ' ----- ';
+    
+    $slack->sendMessages('log-spam-google', $msgs);
+    
+    
+    echo ($article);
     
     die();
+} else {
+    echo ('invalid key');
 }
 
--- spamtonprof.php
+++ spamtonprof.php
@@ -20,7 +20,7 @@
  *
  *
  *
- * Version: 1.1.8.0.3
+ * Version: 1.1.8.0.4
  *
  *
  * Author: yopla
