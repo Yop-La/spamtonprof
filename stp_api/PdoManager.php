d97e27cce84042bb8cf8559d4fca132fef45e3a8
Alexandre <alex.guillemine@gmail.com>
2019-06-03 11:15:08 +0200

    se connecter au postgre de prof resolution probleme client local

--- stp_api/PdoManager.php
+++ stp_api/PdoManager.php
@@ -18,14 +18,27 @@ class PdoManager
         if (is_null(PdoManager::$bdd)) {
 
             try {
+                $probleme_client = false;
+                if (defined('PROBLEME_CLIENT')) {
+                    $probleme_client = true;
+                }
 
-                if (LOCAL) {
+                if (LOCAL && !$probleme_client) {
                     
                     self::$bdd = new PDO('pgsql:host=' . DB_HOST_PG_LOCAL . ';port=5432;application_name=stp;dbname=' . DB_NAME_PG_LOCAL . ';user=' . DB_USER_PG_LOCAL . ';password=' . DB_PASSWORD_PG_LOCAL);
-                
+                    
+                    $slack->sendMessages("log", array(
+                        "LOCAL"
+                    ));
+                    
                 } else {
                     
                     self::$bdd = new PDO('pgsql:host=' . DB_HOST_PG . ';port=5432;application_name=stp;dbname=' . DB_NAME_PG . ';user=' . DB_USER_PG . ';password=' . DB_PASSWORD_PG);
+                    
+                    $slack->sendMessages("log", array(
+                        "PROD"
+                    ));
+                    
                 }
 
                 self::$bdd->exec("SET TIME ZONE 'Europe/Paris';");
@@ -41,5 +54,7 @@ class PdoManager
 
         return self::$bdd;
     }
+    
+
 }
 
