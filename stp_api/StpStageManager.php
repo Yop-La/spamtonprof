4c0b26c74937aaebddcf21b14b0590016440cd3f
Alexandre <alex.guillemine@gmail.com>
2019-06-03 17:41:52 +0200

    ajout propriété test dans add + ajout updateRefProche

--- stp_api/StpStageManager.php
+++ stp_api/StpStageManager.php
@@ -13,8 +13,8 @@ class StpStageManager
 
     public function add(StpStage $stpStage)
     {
-        $q = $this->_db->prepare('insert into stp_stage(ref_proche, ref_eleve, ref_formule, ref_plan, date_debut, date_inscription, remarque_inscription, ref_prof, ref_compte, subs_id, ref_stage, test) values( :ref_proche,:ref_eleve,:ref_formule,:ref_plan,:date_debut,:date_inscription,:remarque_inscription,:ref_prof,:ref_compte,:subs_id,:ref_stage)');
-        $q->bindValue(':ref_proche', $stpStage->getRef_proche());
+        $q = $this->_db->prepare('insert into stp_stage(ref_eleve, ref_formule, ref_plan, date_debut, date_inscription, remarque_inscription, ref_prof, ref_compte, subs_id, test) 
+            values(:ref_eleve,:ref_formule,:ref_plan,:date_debut,:date_inscription,:remarque_inscription,:ref_prof,:ref_compte,:subs_id, :test)');
         $q->bindValue(':ref_eleve', $stpStage->getRef_eleve());
         $q->bindValue(':ref_formule', $stpStage->getRef_formule());
         $q->bindValue(':ref_plan', $stpStage->getRef_plan());
@@ -24,11 +24,19 @@ class StpStageManager
         $q->bindValue(':ref_prof', $stpStage->getRef_prof());
         $q->bindValue(':ref_compte', $stpStage->getRef_compte());
         $q->bindValue(':subs_id', $stpStage->getSubs_id());
-        $q->bindValue(':test', $stpStage->getTest(),\PDO::PARAM_BOOL);
+        $q->bindValue(':test', $stpStage->getTest(), \PDO::PARAM_BOOL);
         $q->execute();
 
         $stpStage->setRef_stage($this->_db->lastInsertId());
 
         return ($stpStage);
+    }
+
+    public function updateRefProche(\spamtonprof\stp_api\StpStage $stage)
+    {
+        $q = $this->_db->prepare("update stp_stage set ref_proche = :ref_proche where ref_stage = :ref_stage");
+        $q->bindValue(":ref_stage", $stage->getRef_stage());
+        $q->bindValue(":ref_proche", $stage->getRef_proche());
+        $q->execute();
     }
 }
