02f29c04683afd0a3e01affae22a6266ba7205f5
Alexandre <alex.guillemine@gmail.com>
2019-07-03 17:43:06 +0200

    annulation de l'email de test du compte gmx ( risque de blocage )

--- hook/getVirginGmxAct.php
+++ hook/getVirginGmxAct.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * 
- *  ppour générer un compte lbc avant publication d'annonces par zenno ( en prod )
+ *  ppour gÃ©nÃ©rer un compte lbc avant publication d'annonces par zenno ( en prod )
  *  
  */
 require_once (dirname(dirname(dirname(dirname(dirname(__FILE__))))) . "/wp-config.php");
@@ -19,43 +19,13 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-
-
 if ($_POST['password'] == HOOK_SECRET) {
 
     $gmxActMg = new \spamtonprof\stp_api\GmxActManager();
-    $slack = new \spamtonprof\slack\Slack();
-    
-    do {
-        
-        $gmxAct = $gmxActMg->get(array(
-            'virgin'
-        ));
-        
-        $email = $gmxAct->getMail();
-        
-        $smtpServer = new \spamtonprof\stp_api\SmtpServer(array(
-            'host' => 'mail.gmx.com',
-            'port' => 587,
-            'password' => $gmxAct->getPassword(),
-            'username' => $email
-        ));
-        
-        $send = $smtpServer->sendEmail('Smtp de ' . $email . ' bien activé', 'soutien.par.mail@gmail.com', 'Smtp de ' . $email . ' bien actif', $email, 'Spammy', false);
-        
-        if (! $send) {
-            
-            $slack->sendMessages('log', array(
-                'Smtp inactif sur ce compte gmx : ' . $email . '. Il faut l\'activer '
-            ));
-            
-            $gmxAct->setSmtp_enabled(false);
-            $gmxActMg->updateHasRedirection($gmxAct);
-        }
-    } while (! $send);
-    
-    
-    
+
+    $gmxAct = $gmxActMg->get(array(
+        'virgin'
+    ));
 
     $ret = new \stdClass();
     $ret->gmx_act = $gmxAct;
