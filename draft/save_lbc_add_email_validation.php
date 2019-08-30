3cb832783e7490f40640369b624d299391b65775
Alexandre <alex.guillemine@gmail.com>
2019-08-30 19:10:57 +0200

    pour stocker les annonces de renouvellement d'annonces en base

--- draft/save_lbc_add_email_validation.php
+++ draft/save_lbc_add_email_validation.php
@@ -25,6 +25,69 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
+
+
+$lbcAccountMg = new \spamtonprof\stp_api\LbcAccountManager();
+
+$gmailMg = new \spamtonprof\googleMg\GoogleManager("mailsfromlbc@gmail.com");
+$msgs = $gmailMg->listMessages('renouvelez gratuitement votre annonce ', 60, 50);
+
+$lbcRenewalUrlManager= new \spamtonprof\stp_api\LbcRenewalUrlManager();
+
+foreach ($msgs as $msg) {
+    
+    $gmail_id = $msg->id;
+    
+    $message = $gmailMg->getMessage($gmail_id, [
+        'format' => 'full'
+    ]);
+    
+   
+    $gmailId = $message->id;
+    
+    $from = $gmailMg->getHeader($message, "From");
+    $snippet = $message->snippet;
+    $subject = $gmailMg->getHeader($message, "Subject");
+    $to = $gmailMg->getHeader($message, "To");
+    $messageId = $gmailMg->getHeader($message, "Message-Id");
+    $body = $gmailMg->getBody($message);
+    
+    
+    
+    $timeStamp = $message->internalDate / 1000;
+    $dateReception = new \DateTime();
+    $dateReception->setTimestamp($timeStamp);
+    $dateReception->setTimezone(new \DateTimeZone('Europe/Paris'));
+    
+    
+    $to = extractFirstMail($to);
+    
+    $urls = extract_url($body);
+    $url = $urls[0][2];
+    
+    $url = htmlspecialchars_decode($url);
+    
+    
+    $act = $lbcAccountMg->get(array(
+        "mail" => $to
+    ));
+    
+    $renewalUrl = $lbcRenewalUrlManager->add(new \spamtonprof\stp_api\LbcRenewalUrl(array(
+        "url" => $url,
+        "statut" => 1,
+        "ref_compte_lbc" => $act->getRef_compte(),
+        "date_reception" => $dateReception->format(PG_DATETIME_FORMAT)
+    )));
+    
+    
+    prettyPrint($renewalUrl)
+}
+
+
+exit();
+
+
+
 $gmailMg = new \spamtonprof\googleMg\GoogleManager("mailsfromlbc@gmail.com");
 $msgs = $gmailMg->listMessages('leboncoin "mise en ligne"', 60, 50);
 
