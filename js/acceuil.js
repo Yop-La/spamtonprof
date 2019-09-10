3db675e308b367a392afc9f788556bb0886b79db
Alexandre <alex.guillemine@gmail.com>
2019-09-10 10:29:05 +0200

    changement message home

--- js/acceuil.js
+++ js/acceuil.js
@@ -2,7 +2,7 @@ jQuery( document ).ready( function( jQuery ) {
 
 	
 	
-	showMessage("Psss, pour attaquer la rentrée sans stress: <a class = 'link_msg' href = 'http://spamtonprof.com/stage-ete/' > c'est là que ça ce passe !</a>")
+	showMessage("Psss, comme c'est la rentrée, on t'offre 7 jours d'essai: <a class = 'link_msg' href = 'http://spamtonprof.com/semaine-decouverte/' > c'est là que ça ce passe !</a>")
 
 //	//debut timer essai	
 //
