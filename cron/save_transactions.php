43017bcc3131c0054da428f2441060f1177ad64d
Alexandre <alex.guillemine@gmail.com>
2020-01-19 00:04:01 +0100

    ajout message + suppression transaction et charge en cas de reprise

--- cron/save_transactions.php
+++ cron/save_transactions.php
@@ -19,8 +19,13 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
-
 define('PROBLEME_CLIENT', true);
+
+$slack = new \spamtonprof\slack\Slack();
+$slack->sendMessages('stripe_transactions', array(
+    '------',
+    'Debut de récupération des transations'
+));
 
 $test_mode = false;
 
@@ -34,33 +39,63 @@ $payouts = $stripePayoutMg->getAll(array(
     'key' => 'no_transactions_status'
 ));
 
+if (count($payouts) == 0) {
+    $slack->sendMessages('stripe_transactions', array(
+        'Aucun payout à traiter ...'
+    ));
+}
+
 foreach ($payouts as $payout) {
-    
+
+    $stripeTransactionMg->deleteAll(array(
+        'key' => $stripeTransactionMg::by_ref_payout,
+        'params' => array(
+            'ref_payout' => $payout->getRef()
+        )
+    ));
+
+    $stripeChargeManagerMg->deleteAll(array(
+        'key' => $stripeChargeManagerMg::not_referenced_by_stripe_transaction
+    ));
+
     $prof = $profMg->get(array(
         'ref_prof' => $payout->getRef_prof()
     ));
-    
+
     $stripe_prof = new \spamtonprof\stp_api\StripeManager($test_mode, $prof->getEmail_stp());
-    
+
     $all_transactions = $stripe_prof->list_balance_transaction($payout->getRef_stripe());
-    
-    if(!$all_transactions || count($all_transactions) == 0){
+
+    $nb_transactions = 0;
+
+    if ($all_transactions) {
+        $nb_transactions = count($all_transactions);
+    }
+    $slack->sendMessages('stripe_transactions', array(
+        $nb_transactions . " à traiter pour le payout: " . $payout->getRef_stripe()
+    ));
+
+    if (! $all_transactions || $nb_transactions == 0) {
+
+        $slack->sendMessages('stripe_transactions', array(
+            '-->>    Impossible de récupérer les transactions de ce payout:' . $payout->getRef_stripe() . "    <<--"
+        ));
+
         $payout->setTransactions_status($payout::cant_retrieve_transactions);
-        $stripePayoutMg-> update_transactions_status($payout);
+        $stripePayoutMg->update_transactions_status($payout);
         continue;
     }
-    
+
     $payout->setTransactions_status($payout::not_all_transactions_retrieved);
-    $stripePayoutMg-> update_transactions_status($payout);
-    
-    
+    $stripePayoutMg->update_transactions_status($payout);
+
     foreach ($all_transactions as $transaction) {
-        
+
         $transaction = $stripe_prof->retrieve_balance_transaction($transaction->id);
-        
+
         $available_on = new \DateTime();
         $available_on->setTimestamp($transaction->available_on);
-        
+
         $stripeTransaction = new \spamtonprof\stp_api\StripeTransaction(array(
             "transaction_id" => $transaction->id,
             "transaction_amount" => $transaction->amount,
@@ -69,15 +104,15 @@ foreach ($payouts as $payout) {
             "available_on" => $available_on->format(PG_DATETIME_FORMAT),
             "type" => $transaction->type
         ));
-        
+
         $stripeTransactionMg->add($stripeTransaction);
-        
+
         $charge = $stripe_prof->retrieve_source_charge_of_transaction($transaction->id);
         if ($charge) {
-            
+
             $created = new \DateTime();
             $created->setTimestamp($charge->created);
-            
+
             $stripeCharge = new \spamtonprof\stp_api\StripeCharge(array(
                 "ref_stripe" => $charge->id,
                 "amount" => $charge->amount,
@@ -85,19 +120,20 @@ foreach ($payouts as $payout) {
                 "customer" => $charge->customer,
                 'invoice' => $charge->invoice
             ));
-            
+
             $stripeChargeManagerMg->add($stripeCharge);
-            
+
             $stripeTransaction->setRef_charge($stripeCharge->getRef());
             $stripeTransactionMg->update_ref_charge($stripeTransaction);
         }
     }
-    
+
     $payout->setTransactions_status($payout::transactions_retrieved);
-    $stripePayoutMg-> update_transactions_status($payout);
-    
-    
-    
+    $stripePayoutMg->update_transactions_status($payout);
 }
+
+$slack->sendMessages('stripe_transactions', array(
+    'Fin de récupération des transations'
+));
 
 exit();
