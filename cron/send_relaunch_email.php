e3007ed56f8f002dbd0ff28a4613119e8044fd8c
Alexandre <alex.guillemine@gmail.com>
2020-04-08 20:47:26 +0200

    new comments

--- cron/send_relaunch_email.php
+++ cron/send_relaunch_email.php
@@ -14,6 +14,7 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
+// en prod. Toutes les heures
 /* cron d'envoi des emails de relance */
 
 $aboMg = new \spamtonprof\stp_api\StpAbonnementManager();
