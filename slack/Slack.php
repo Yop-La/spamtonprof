550c91c3d3c5489908ab49f42e377f782b8969eb
Alexandre <alex.guillemine@gmail.com>
2020-07-04 16:03:55 +0200

    add search function

--- slack/Slack.php
+++ slack/Slack.php
@@ -10,6 +10,7 @@ class Slack
 
 {
 
+    // https://github.com/wrapi/slack
     const LogLbc = "log-lbc", Log = "log", Abonnement = "abonnement", Invoicing = "invoicing", MessagEleve = "message-eleve";
 
     private $slack;
@@ -22,22 +23,23 @@ class Slack
     public function sendMessages(string $channel, array $msgs)
     {
         $sum = array_map("strlen", $msgs);
-        
+
         $sum = array_sum($sum);
-        
+
         $nbSplits = ceil($sum / 20000);
-        
-      
+
         if ($nbSplits > 1) {
             $array_msgs = array_chunk($msgs, $nbSplits);
         } else {
-            $array_msgs = array($msgs);
+            $array_msgs = array(
+                $msgs
+            );
         }
-        
+
         $res = [];
-        
+
         foreach ($array_msgs as $msgs) {
-            
+
             $response = $this->slack->chat->postMessage(array(
                 "channel" => "#" . $channel,
                 "text" => implode("\n", toUtf8($msgs)),
@@ -48,10 +50,20 @@ class Slack
                 "unfurl_links" => true,
                 "unfurl_media" => false
             ));
-            
+
             $res[] = $response;
         }
-        
+
         return ($res);
+    }
+
+    function search($query)
+    {
+        $response = $this->slack->search->messages(array(
+            "query" => $query,
+            "count" => 50
+        ));
+
+        return ($response);
     }
 }
