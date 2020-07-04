7fd6c83c32f3ec2ffd8e27976e675c855418c30b
Alexandre <alex.guillemine@gmail.com>
2020-07-04 16:43:48 +0200

    a class to import & export lbc title to complete

---
+++ stp_api/LbcTitleController.php
@@ -0,0 +1,18 @@
+<?php
+namespace spamtonprof\stp_api;
+
+class LbcTitleController
+
+{
+
+    private $_db;
+
+    // Instance de PDO
+    public function __construct()
+
+    {
+        
+        
+        
+    }
+}
